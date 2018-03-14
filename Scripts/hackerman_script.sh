8fa2d1d005f64060e98239e78f143e0079cb179c
NJ Pelpinosas <n3st3r12345@gmail.com>
2018-03-13 20:58:53 -0700

    added cmus to configuration

--- Scripts/hackerman_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-killhm=0
-pgrep -f 'hackerman-music'
-if [ $? -eq 1 ]; then
-    exec -a 'hackerman-music' mpv "$HOME/Music/Hackerman_Theme.opus" &
-    killhm=1
-fi
-lxterminal ; if [ $killhm -eq 1 ]; then pkill -f 'hackerman-music'; fi
---
+++ Scripts/music_check_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -z $(ps --no-headers -o comm= -C cmus) ]; then
+lxterminal -t cmus -e cmus
+else
+cmus-remote -u
+fi
