2d785c61329b28c1e6bb099abf55eae4f9fb58a9
NJ Pelpinosas <n3st3r12345@gmail.com>
2018-04-19 11:04:31 -0700

    moved Thunderbird to internet_script.sh

--- .scripts/internet_script.sh
+++ .scripts/internet_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-choices="Waterfox\nElinks\nGoogle Chrome"
+choices="Waterfox\nElinks\nGoogle Chrome\nThunderbird"
 
-chosen=$(echo -e "$choices" | rofi -dmenu -i -p "###-Choose a Web Browser-###")
+chosen=$(echo -e "$choices" | rofi -dmenu -i -p "###-Choose an Internet Client-###")
 
 case "$chosen" in
 	Waterfox) notify-send "Running Waterfox" && waterfox ;;
 	Elinks) notify-send "Running Elinks" && lxterminal -e elinks duckduckgo.com ;;
 	Google\ Chrome) notify-send "Running Chrome" && google-chrome-stable ;;
+	Thunderbird) notify-send "Running Thunderbird Email" && thunderbird ;;
 esac
 	
